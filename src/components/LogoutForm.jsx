3f182360abbbe823a7b0d49e54cab2d4c52729f3
hijera <hi-jera@yandex.ru>
2020-06-22 22:41:10 +0300

    Фикс

--- src/components/LogoutForm.jsx
+++ src/components/LogoutForm.jsx
@@ -17,7 +17,7 @@ function LogoutForm(props) {
             <form onSubmit={handleSubmit}>
                 Hello, {profile.name ? profile.name : 'Guest'}
                 <span className="offset-item">
-                    <img src={profile.avatar} className="avatar"/>
+                    <img src={profile.avatar} className="avatar" alt={"avatar"} />
                 </span>
                 <button type="btn btn-outline-success my-2 my-sm-0" className="btn btn-danger">Logout</button>
             </form>
